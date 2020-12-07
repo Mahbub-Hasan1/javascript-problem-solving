c78ac860da9c90384b07342b9665002fca1a8a21
Md-Mahbub-Hasan <trylife.mahbub@gmail.com>
2020-12-07 04:53:27 -0800

    problem arraySum 5

---
+++ arraySum.js
@@ -0,0 +1,8 @@
+var numbers = [45, 65, 78, 12, 3, 54, 65];
+var sum = 0;
+
+for(var i = 0; i < numbers.length; i++){
+    var element = numbers[0];
+    sum = sum + element;
+}
+console.log("total of the numbers: ", sum);
